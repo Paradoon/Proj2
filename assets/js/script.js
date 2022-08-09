3e9e5f16c7794d0e1f81123ac90f1d237adb56e2
Paradoon <j_ingvarsson@hotmail.com>
2022-08-09 09:59:58 +0000

    added bricks

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,6 +10,38 @@ let x = canvas.width/2;
 let y = canvas.height-30;
 let dx = 2;
 let dy = -2;
+var brickRowCount = 3;
+var brickColumnCount = 5;
+var brickWidth = 75;
+var brickHeight = 20;
+var brickPadding = 10;
+var brickOffsetTop = 30;
+var brickOffsetLeft = 30;
+
+
+var bricks = [];
+for(var c=0; c < brickColumnCount; c++) {
+    bricks[c] = [];
+    for(var r=0; r < brickRowCount; r++) {
+        bricks[c][r] = { x: 0, y: 0, status: 1 };
+    }
+}
+
+function drawBricks() {
+    for(var c=0; c<brickColumnCount; c++) {
+        for(var r=0; r<brickRowCount; r++) {
+            var brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
+            var brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
+            bricks[c][r].x = brickX;
+            bricks[c][r].y = brickY;
+            ctx.beginPath();
+            ctx.rect(brickX, brickY, brickWidth, brickHeight);
+            ctx.fillStyle = "#0095DD";
+            ctx.fill();
+            ctx.closePath();
+        }
+    }
+}
 
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
@@ -50,28 +82,43 @@ function draw() {
     ctx.clearRect (0, 0, canvas.width, canvas.height);
     drawball();
     drawPaddle();
-    if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
-        dy = -dy;
-    }
-    
+    drawBricks();
     if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
         dx = -dx;
     }
+    if(y + dy < ballRadius) {
+        dy = -dy;
+    } else if (y + dy > canvas.height-ballRadius) {
+        if(x > paddleX && x < paddleX + paddleWidth) {
+            dy = -dy;
+        } 
+        else {
+            alert("GAME OVER");
+            document.location.reload();
+            clearInterval(interval);
+    }
+}
 
-    if(rightPressed) {
+    if(rightPressed && paddleX < canvas.width-paddleWidth) {
         paddleX += 7;
-        if (paddleX + paddleWidth > canvas.width) {
-            paddleX = canvas.width - paddleWidth;
-        }
-    } else if (leftPressed) {
+    }
+    else if (leftPressed && paddleX > 0) {
         paddleX -= 7;
-        if (paddleX < 0) {
-            paddleX = 0;
-        }
     }
 
     x += dx;
     y += dy;
 }
 
-setInterval(draw, 10);
+function colissionDetection() {
+    for (let c = 0; c < brickColumnCount; c++) {
+        for(let r = 0; r < brickRowCount; r++) {
+            const b = bricks[c][r];
+            if (x > b.x && x < b.x+brickWidth && y > b.y && y < b.y+brickHeight) {
+                dy = -dy;
+            }
+        }
+    }
+}
+
+var interval = setInterval(draw, 10);
